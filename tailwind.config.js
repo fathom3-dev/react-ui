9afd99086e974a8126679b4f8bf088f6965c8045
DonsWayo <34981526+DonsWayo@users.noreply.github.com>
2022-04-06 10:12:47 +0200

    chore: tailwind config align tailwind 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './stories/*'],
-  darkMode: false,
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './stories/*'],
   theme: {
     extend: {},
   },
